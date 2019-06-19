5f38d852477a43a0ffd35c0817d9b7f4bb5cb10e
Samantha Kane <s.kane133@gmail.com>
2019-06-19 21:07:50 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,7 @@ class Hash
   def keys_of(arguments)
     array = []
     arguments.each do |idk|
-      idk.each do |key, value|
+      .each do |key, value|
         array << value
       end
   end
